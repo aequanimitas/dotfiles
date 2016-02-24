9f15d14e1771138ed4d2e51b8997927c7b10755c
aequanimitas <htavenido@gmail.com>
2016-02-24 08:58:02 +0800

    code setup

---
+++ repo-setup.sh
@@ -0,0 +1,3 @@
+for i in justmath algo sentire sleepsnear dotfiles tedious snips taradayo lagas karasa curo sicp-redux psex endo chow math-notes vaporwhere; do 
+	git clone git@github.com:aequanimitas/$i.git;
+done
