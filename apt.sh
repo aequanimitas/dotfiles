a35d0520a56187fe3952a95cbb10832fa3887688
aequanimitas <htavenido@gmail.com>
2016-02-24 10:20:44 +0800

    - remove npm installation via package manager - node project dependencies

--- apt.sh
+++ apt.sh
@@ -1,3 +1,3 @@
-for i in tmux git bash-completion postgresql postgresql-contrib npm emacs; do
+for i in tmux git bash-completion postgresql postgresql-contrib emacs; do
 	apt-get install $i;
 done;
--- repo-setup.sh
+++ repo-setup.sh
@@ -1,3 +1,13 @@
 for i in justmath algo sentire sleepsnear dotfiles tedious snips taradayo lagas karasa curo sicp-redux psex endo chow math-notes vaporwhere; do 
 	git clone git@github.com:aequanimitas/$i.git;
 done
+
+for i in $(ls); do
+	if [ -e  "$i/package.json" ];
+	then 
+		echo "Installing $i dependencies..."
+		cd $i
+		npm install
+		cd ..
+	fi
+done
